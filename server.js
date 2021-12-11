767743a694f79ec0cc813767be263438f64094a8
vikas kumar <vkk1310@gmail.com>
2021-12-12 01:33:22 +0530

    error cors

--- server.js
+++ server.js
@@ -5,15 +5,8 @@ const cors = require("cors")
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 // DB config
 const db = require("./config/keys").mongoURI; // change to this process.env.MONGODB_URL
