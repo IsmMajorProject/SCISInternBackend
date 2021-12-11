acb6ce45d02a0b7422da12b56037fe84e79c56b4
Lucky Tolani <lakhitolani41@gmail.com>
2021-12-12 02:39:50 +0530

    env variables deleted for prod

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-// Rename to keys.js
-
-module.exports = {
-  mongoURI: "mongodb+srv://jobportal:jobportal@cluster0.hpmgv.mongodb.net/JobPortal?retryWrites=true&w=majority",
-  jwtSecret: "lmaosomething",
-};
